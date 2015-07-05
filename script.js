030557c6e8ab5e7d9d1cf0e8cc5c12dc94620feb
Ire Aderinokun <ire@ireaderinokun.com>
2015-07-05 08:38:08 +0100

    finishing

--- script.js
+++ script.js
@@ -1,92 +1,113 @@
 $(document).ready(function() {
 
 
+    // Remove no-js class
+    $('html').removeClass('no-js');
 
 	$('#toggleMenu').on('click', function() {
 
-        if ( $(this).hasClass('toggleMenu--open') ) {
+        if ( $(this).hasClass('js-open') ) {
 
             $('#nav > ul > li:not(#toggleMenu)').removeClass('js-showElement');
-            $('#nav > ul > li:not(#toggleMenu)').addClass('js-hideElement');
-            $(this).removeClass('toggleMenu--open');
+            $(this).removeClass('js-open');
 
         } else {
 
-            $('#nav > ul > li:not(#toggleMenu)').removeClass('js-hideElement');
             $('#nav > ul > li:not(#toggleMenu)').addClass('js-showElement');
-            $(this).addClass('toggleMenu--open');
+            $(this).addClass('js-open');
 
         }
 
-		return false;
-
+		return false; 
 	})
 
-
-	// Disappear submenu
-	// ------------------------
-	$('body').on('touchend click', function() {
-
-    	// If user taps or clicks on screen, hide the sub menus
-    	//$('li.hasSubMenu > ul').css('display', 'none'); 
-
-    	// ISSUE ON TAP - clicking on
-    });
-
-    $('li.hasSubMenu').on('mouseleave', function() {
-
-    	$(this).children('ul').removeClass('js-showElement');
-        $(this).children('ul').addClass('js-hideElement');
-
-    });
+    // Add plus mark to li that have a sub menu
+    $('li:has("ul") > a').append('<span class="plusMark">+</span>');
 
 
 
 
-
-    // Show submenu
+    // sub menu
 	// ------------------------
 
-    $('li.hasSubMenu').on('mouseover keyup click', function(e) {
+    // When interacting with a li that has a sub menu
+    $('li:has("ul")').on('mouseover keyup click mouseleave', function(e) {
 
     	// If either -
-    		// tabbing onto an element that has a subMenu
-    		// hovering over an element that has a subMenu
-    		// clicking on an element that has a subMenu
-    	if ( e.keyCode === 9 | e.type === 'mouseover' | e.type === 'click' ) {
-    		
-    		// Show subMenu
+    		// tabbing into the li that has a sub menu
+    		// hovering over the li that has a sub menu
+    	if ( e.keyCode === 9 | e.type === 'mouseover' ) {
+
+    		// Show sub menu
     		$(this).children('ul').removeClass('js-hideElement');
             $(this).children('ul').addClass('js-showElement');
-    		return false;
     	}
+
+		// If mouse leaves li that has sub menu
+		if ( e.type === 'mouseleave' ) {
+
+			// hide sub menu
+			$(this).children('ul').removeClass('js-showElement');
+            $(this).children('ul').addClass('js-hideElement');
+		}
+
+
+		// If clicking on li that has a sub menu
+		if ( e.type === 'click' ) {
+
+			// If sub menu is already open
+			if ( $(this).children('a').hasClass('js-openSubMenu') ) {
+
+                // remove Open class
+				$(this).children('a').removeClass('js-openSubMenu');
+
+				// Hide sub menu
+	    		$(this).children('ul').removeClass('js-showElement');
+	            $(this).children('ul').addClass('js-hideElement');
+
+
+			// If sub menu is closed
+			} else {
+
+                // add Open class
+				$(this).children('a').addClass('js-openSubMenu');
+
+				// Show sub menu
+	    		$(this).children('ul').removeClass('js-hideElement');
+	            $(this).children('ul').addClass('js-showElement');
+
+			}
+
+			return false;
+
+		} // end click event
 
     });
 
 
-    // Tabbing through Levels of Submenu
+    // Tabbing through Levels of sub menu
 	// ------------------------
-    
 
-    $('li.hasSubMenu > ul > li:last-child > a').on('keydown', function(e) {
-
-    	// If tabbing out of the last link in a submenu AND not tabbing into another menu
-    	if ( (e.keyCode == 9) && !$(this).parent('li').hasClass('hasSubMenu') ) {
+    // If key is pressed while on the last link in a sub menu
+    $('li > ul > li:last-child > a').on('keydown', function(e) {
 
 
-    		// If tabbing out of a third level and there are no other links in the parent subMenu
-    		if ( $(this).parent('li').parent('ul').parent('li').parent('ul').parent('li').hasClass('hasSubMenu') 
+    	// If tabbing out of the last link in a sub menu AND not tabbing into another sub menu
+    	if ( (e.keyCode == 9) && $(this).parent('li').children('ul').length == 0 ) {
+
+				// Close this sub menu
+        		$(this).parent('li').parent('ul').removeClass('js-showElement');
+                $(this).parent('li').parent('ul').addClass('js-hideElement');
+
+
+    		// If tabbing out of a third level sub menu and there are no other links in the parent (level 2) sub menu
+    		if ( $(this).parent('li').parent('ul').parent('li').parent('ul').parent('li').children('ul').length > 0 
     			 && $(this).parent('li').parent('ul').parent('li').is(':last-child') ) {
 
-    				// Close the parent submenu (level 2)
+    				// Close the parent sub menu (level 2) as well
     				$(this).parent('li').parent('ul').parent('li').parent('ul').removeClass('js-showElement');
                     $(this).parent('li').parent('ul').parent('li').parent('ul').addClass('js-hideElement');
-    		} 
-
-
-    		// Close this submenu
-    		$(this).parent('li').parent('ul').removeClass('js-showElement');
-            $(this).parent('li').parent('ul').addClass('js-hideElement');
+    		}
 
     	}
 
